eda7a34fd9d9dd944c0e88efbca660f1b4efb976
guptaaryan1 <acgupta6@gmail.com>
2025-10-26 10:52:02 -0500

    complete revamp, changed stacks, setup backend and placeholder frontend

---
+++ love-dice/frontend/src/pages/PlaceBets.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlaceBets = () => {
+
+  return (
+    <div className="min-h-screen bg-black relative">
+      placebets page
+    </div>
+  );
+};
+
+export default PlaceBets;
